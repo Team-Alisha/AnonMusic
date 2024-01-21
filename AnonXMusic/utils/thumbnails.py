1955cae3c1aecbe70ebd7159742aa7a89b4c1ab7
TEAM ALISHA <116377135+Team-Alisha@users.noreply.github.com>
2024-01-21 16:01:48 +0530

    Update thumbnails.py
    Signed-off-by: TEAM ALISHA <116377135+Team-Alisha@users.noreply.github.com>

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -9,7 +9,7 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 from AnonXMusic import app
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from config import YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
