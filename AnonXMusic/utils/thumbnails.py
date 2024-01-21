e3b32c810e66634879e17b88a231056e0a05bd33
TEAM ALISHA <116377135+Team-Alisha@users.noreply.github.com>
2024-01-21 16:08:15 +0530

    Update thumbnails.py
    Signed-off-by: TEAM ALISHA <116377135+Team-Alisha@users.noreply.github.com>

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -1,13 +1,12 @@
 import os
 import re
-import textwrap
 import numpy as np
 import aiofiles
 import aiohttp
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
-from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
-                 ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
+
 from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
 
